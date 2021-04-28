50ece34ba4ff8dbda8edda9a9363a9672cbc0267
Neraren <81916677+Neraren@users.noreply.github.com>
2021-04-28 23:27:43 +0800

    Add files via upload

---
+++ Project_TALA_2.0/MediaPlayerUI/Form3.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.button6 = New System.Windows.Forms.Button()
+        Me.button7 = New System.Windows.Forms.Button()
+        Me.button8 = New System.Windows.Forms.Button()
+        Me.button9 = New System.Windows.Forms.Button()
+        Me.button5 = New System.Windows.Forms.Button()
+        Me.dataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'button6
+        '
+        Me.button6.BackColor = System.Drawing.Color.FromArgb(CType(CType(24, Byte), Integer), CType(CType(22, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.button6.FlatAppearance.BorderSize = 0
+        Me.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button6.ForeColor = System.Drawing.Color.LightGray
+        Me.button6.Location = New System.Drawing.Point(40, 125)
+        Me.button6.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.button6.Name = "button6"
+        Me.button6.Size = New System.Drawing.Size(225, 62)
+        Me.button6.TabIndex = 25
+        Me.button6.Text = "button6"
+        Me.button6.UseVisualStyleBackColor = False
+        '
+        'button7
+        '
+        Me.button7.BackColor = System.Drawing.Color.FromArgb(CType(CType(24, Byte), Integer), CType(CType(22, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.button7.FlatAppearance.BorderSize = 0
+        Me.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button7.ForeColor = System.Drawing.Color.LightGray
+        Me.button7.Location = New System.Drawing.Point(40, 195)
+        Me.button7.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.button7.Name = "button7"
+        Me.button7.Size = New System.Drawing.Size(225, 62)
+        Me.button7.TabIndex = 24
+        Me.button7.Text = "button7"
+        Me.button7.UseVisualStyleBackColor = False
+        '
+        'button8
+        '
+        Me.button8.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.button8.BackColor = System.Drawing.Color.FromArgb(CType(CType(24, Byte), Integer), CType(CType(22, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.button8.FlatAppearance.BorderSize = 0
+        Me.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button8.ForeColor = System.Drawing.Color.LightGray
+        Me.button8.Location = New System.Drawing.Point(735, 555)
+        Me.button8.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.button8.Name = "button8"
+        Me.button8.Size = New System.Drawing.Size(225, 62)
+        Me.button8.TabIndex = 23
+        Me.button8.Text = "button8"
+        Me.button8.UseVisualStyleBackColor = False
+        '
+        'button9
+        '
+        Me.button9.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.button9.BackColor = System.Drawing.Color.FromArgb(CType(CType(235, Byte), Integer), CType(CType(42, Byte), Integer), CType(CType(83, Byte), Integer))
+        Me.button9.FlatAppearance.BorderSize = 0
+        Me.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button9.ForeColor = System.Drawing.Color.LightGray
+        Me.button9.Location = New System.Drawing.Point(501, 555)
+        Me.button9.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.button9.Name = "button9"
+        Me.button9.Size = New System.Drawing.Size(225, 62)
+        Me.button9.TabIndex = 22
+        Me.button9.Text = "button9"
+        Me.button9.UseVisualStyleBackColor = False
+        '
+        'button5
+        '
+        Me.button5.FlatAppearance.BorderSize = 0
+        Me.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(235, Byte), Integer), CType(CType(42, Byte), Integer), CType(CType(83, Byte), Integer))
+        Me.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button5.ForeColor = System.Drawing.Color.LightGray
+        Me.button5.Location = New System.Drawing.Point(0, 0)
+        Me.button5.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.button5.Name = "button5"
+        Me.button5.Size = New System.Drawing.Size(38, 38)
+        Me.button5.TabIndex = 21
+        Me.button5.Text = "X"
+        Me.button5.UseVisualStyleBackColor = True
+        '
+        'dataGridView1
+        '
+        Me.dataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataGridView1.Location = New System.Drawing.Point(310, 125)
+        Me.dataGridView1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.dataGridView1.Name = "dataGridView1"
+        Me.dataGridView1.RowHeadersWidth = 62
+        Me.dataGridView1.Size = New System.Drawing.Size(650, 422)
+        Me.dataGridView1.TabIndex = 20
+        '
+        'Form3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(45, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1026, 663)
+        Me.Controls.Add(Me.button6)
+        Me.Controls.Add(Me.button7)
+        Me.Controls.Add(Me.button8)
+        Me.Controls.Add(Me.button9)
+        Me.Controls.Add(Me.button5)
+        Me.Controls.Add(Me.dataGridView1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form3"
+        Me.Text = "Form3"
+        CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Private WithEvents button6 As Button
+    Private WithEvents button7 As Button
+    Private WithEvents button8 As Button
+    Private WithEvents button9 As Button
+    Private WithEvents button5 As Button
+    Private WithEvents dataGridView1 As DataGridView
+End Class
